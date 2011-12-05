2d54ae97c8931dba5f87fd5095d211ee23435efd
lreyero <lreyero@44a6b4fc-d1bf-9eb8-79f6-09121c05ef92>
2011-12-05 18:07:53 +0000

    modificaciones, pero no finales, aun debatimos que hacer con javaCC

--- Proyecto/src/compilador/lexico/AnalizadorLexico.java
+++ Proyecto/src/compilador/lexico/AnalizadorLexico.java
@@ -1,7 +1,34 @@
 package compilador.lexico;
-
 import compilador.lexico.tokens.*;
 
+/**
+ * 
+ * @author Grupo 1
+ *
+ */
 public class AnalizadorLexico {
 
+	/**
+	 * Atributo de la clase que identifica el estado del automata en el que nos encontramos
+	 */
+	private int estado;
+	
+	/**
+	 * 
+	 */
+	private char buff;
+	
+	/**
+	 * 
+	 */
+	private String lex;
+	
+	/**
+	 * 
+	 */
+	private static int numLinea = 1;
+	
+
+    
+    
 }

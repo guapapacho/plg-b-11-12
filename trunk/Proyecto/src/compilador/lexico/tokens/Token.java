b819e2721499d230b659cc3c1c0596e312562ee0
x.alina.y@gmail.com <x.alina.y@gmail.com@44a6b4fc-d1bf-9eb8-79f6-09121c05ef92>
2011-12-05 17:00:48 +0000

    enumerado tipo token

--- trunk/Proyecto/src/compilador/lexico/tokens/Token.java
+++ trunk/Proyecto/src/compilador/lexico/tokens/Token.java
@@ -1,5 +1,13 @@
 package compilador.lexico.tokens;
 
 public class Token {
-
+    
+    public enum Tipo {
+        EOF, LIT_CARACTER, LIT_CADENA, ID, 
+        NUM_REAL, NUM_ENTERO, SEPARADOR, 
+        OP_ARITMETICO, OP_LOGICO, OP_COMPARACION, OP_ASIGNACION
+    };
+    
+    private Tipo tipo;
+    private Object argumento;
 }

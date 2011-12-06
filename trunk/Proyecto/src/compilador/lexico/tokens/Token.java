10d1e9930445a8684e2bcf1d9d9e94a7e0154a4c
lreyero <lreyero@44a6b4fc-d1bf-9eb8-79f6-09121c05ef92>
2011-12-06 11:02:07 +0000

    Aniado los enumerados para los tipos de tokens que tenemos

--- trunk/Proyecto/src/compilador/lexico/tokens/Token.java
+++ trunk/Proyecto/src/compilador/lexico/tokens/Token.java
@@ -9,18 +9,59 @@ public class Token {
     
 
     /**
-     * 
+     * Tipos de tokens
      */
-	public enum Tipo {
+	public enum TipoToken {
         EOF, LIT_CARACTER, LIT_CADENA, PAL_RESERVADA, 
         IDENTIFICADOR, NUM_REAL, NUM_ENTERO, SEPARADOR, 
         OP_ARITMETICO, OP_LOGICO, OP_COMPARACION, OP_ASIGNACION
-    };
+    }
+    
+    /**
+     * Separadores
+     */
+    public enum Separadores {
+    	PUNTO, PUNTO_COMA, ABRE_LLAVE, CIERRA_LLAVE, ABRE_CORCHETE,
+    	CIERRA_CORCHETE, ALMOHADILLA, DOBLE_ALMOHADILLA, ABRE_PARENTESIS,
+    	CIERRA_PARENTESIS, MENOS_DOSPUNTOS, DOSPUNTOS_MAYOR, MENOR_PORCENTAJE,
+    	PORCENTAJE_MAYOR, PORCENTAJE_DOSPUNTOS, PORCENTAJE_DOSPUNTOS_BIS
+    }
+    
+    /**
+     * Operadores aritmeticos
+     */
+    public enum OpAritmetico {
+    	SUMA, RESTA, INCREMENTO, DECREMENTO, MULTIPLICACION, DIVISION, PORCENTAJE
+    }
+    
+    /**
+     * Operadores de comparacion
+     */
+    public enum OpComparacion {
+    	IGUAL, DISTINTO, MENOR, MAYOR, MENOR_IGUAL, MAYOR_IGUAL
+    }
+    
+    /**
+     * Operadores logicos
+     */
+    public enum OpLogico {
+    	Y, O, NO, Y_BIT, O_BIT, SOBRERO, CIRCUNFLEJO, DOS_MENORES, DOS_MAYORES,			//Revisar!!!! no se si se podrian poner menos porque
+    	AND, ANDEQ, BIT_AND, BIT_OR, COMPL, NOT, NOT_EQ, OR, OR_EQ, XOR, XOR_EQ			//sea el mismo significado...
+    }
+    
+    /**
+     * Operadores de asignacion
+     */
+    public enum OpAsignacion {
+    	ASIGNACION, MAS_IGUAL, MENOS_IGUAL, POR_IGUAL, DIV_IGUAL, PORCENTAJE_IGUAL,
+    	CIRCUNFLEJO_IGUAL, AND_IGUAL, OR_IGUAL, MAYOR_MAYOR_IGUAL, MENOR_MENOR_IGUAL,
+    	PUNTERO
+    }
     
     /**
      * Atributo de la clase que identifica el tipo de token segun el enumerado asociado
      */
-    private Tipo tipo;
+    private TipoToken tipo;
     
     /**
      * Atributo de la clase que identifica el atributo del token
@@ -44,7 +85,7 @@ public class Token {
      * @param numlinea
      * @param numcolumna
      */
-	public Token(Tipo tipo, Object atributo) {
+	public Token(TipoToken tipo, Object atributo) {
 		this.tipo = tipo;
 		this.atributo = atributo;
 	}
@@ -53,7 +94,7 @@ public class Token {
      * 
      * @return
      */
-	public Tipo getTipo() {
+	public TipoToken getTipo() {
 		return tipo;
 	}
 
@@ -61,7 +102,7 @@ public class Token {
 	 * 
 	 * @param tipo
 	 */
-	public void setTipo(Tipo tipo) {
+	public void setTipo(TipoToken tipo) {
 		this.tipo = tipo;
 	}
 	

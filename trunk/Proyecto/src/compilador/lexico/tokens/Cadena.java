7d2ed33688ae043270e667c584bb7dc999b79907
tinkaris <tinkaris@44a6b4fc-d1bf-9eb8-79f6-09121c05ef92>
2011-12-06 09:20:18 +0000

    Le he quitado los errores que habia :P

--- trunk/Proyecto/src/compilador/lexico/tokens/Cadena.java
+++ trunk/Proyecto/src/compilador/lexico/tokens/Cadena.java
@@ -2,4 +2,9 @@ package compilador.lexico.tokens;
 
 public class Cadena extends Token{
 
+	public Cadena(Tipo tipo, Object atributo, int numlinea, int numcolumna) {
+		super(tipo, atributo);
+		// TODO Auto-generated constructor stub
+	}
+
 }

f42c1d940f744a08ecead6c186c3567f6a983166
x.alina.y <x.alina.y@44a6b4fc-d1bf-9eb8-79f6-09121c05ef92>
2011-12-08 17:54:33 +0000

    token he quitado los atributos numlinea y numcolumna y unos separadores que eran equivalentes a otros (por ejemplo el "%>" era la "}")

--- trunk/Proyecto/src/compilador/lexico/tokens/Token.java
+++ trunk/Proyecto/src/compilador/lexico/tokens/Token.java
@@ -24,8 +24,7 @@ public class Token {
     public enum Separadores {
     	PUNTO("."),PUNTO_COMA(";"), ABRE_LLAVE("{"), CIERRA_LLAVE("}"), ABRE_CORCHETE("["),
     	CIERRA_CORCHETE("]"), ALMOHADILLA("#"), DOBLE_ALMOHADILLA("##"), ABRE_PARENTESIS("("),
-    	CIERRA_PARENTESIS(")"), MENOS_DOSPUNTOS("<:"), DOSPUNTOS_MAYOR(":>"), MENOR_PORCENTAJE("<%"),
-    	PORCENTAJE_MAYOR("%>"), PORCENTAJE_DOSPUNTOS("%:"), PORCENTAJE_DOSPUNTOS_BIS("%:%:");
+    	CIERRA_PARENTESIS(")"), DOBLE_DOSPUNTOS("::");
     	
     	private String description;
     	private Separadores(String desc) { description = desc; };
@@ -73,22 +72,11 @@ public class Token {
      */
     private Object atributo;
     
-    /**
-     * Numero de linea en el que se encuentra el token
-     */
-    private int numlinea = 1;
-    
-    /**
-     * Numero de columna en la que se encuentra el token
-     */
-    private int numcolumna = 0;
     
     /**
      * Constructora de la clase
      * @param tipo
      * @param atributo
-     * @param numlinea
-     * @param numcolumna
      */
 	public Token(TipoToken tipo, Object atributo) {
 		this.tipo = tipo;
@@ -96,66 +84,19 @@ public class Token {
 	}
 	
     /**
-     * 
-     * @return
+     * Getter token
+     * @return el tipo del token
      */
 	public TipoToken getTipo() {
 		return tipo;
 	}
-
-	/**
-	 * 
-	 * @param tipo
-	 */
-	public void setTipo(TipoToken tipo) {
-		this.tipo = tipo;
-	}
 	
 	/**
-	 * 
-	 * @return
+	 * Getter token
+	 * @return el atributo del token
 	 */
 	public Object getAtributo() {
 		return atributo;
 	}
 	
-	/**
-	 * 
-	 * @param argumento
-	 */
-	public void setAtribto(Object atributo) {
-		this.atributo = atributo;
-	}
-	
-	/**
-	 * 
-	 * @return
-	 */
-	public int getNumlinea() {
-		return numlinea;
-	}
-	
-	/**
-	 * 
-	 * @param numlinea
-	 */
-	public void setNumlinea(int numlinea) {
-		this.numlinea = numlinea;
-	}
-	
-	/**
-	 * 
-	 * @return
-	 */
-	public int getNumcolumna() {
-		return numcolumna;
-	}
-	
-	/**
-	 * 
-	 * @param numcolumna
-	 */
-	public void setNumcolumna(int numcolumna) {
-		this.numcolumna = numcolumna;
-	}
 }

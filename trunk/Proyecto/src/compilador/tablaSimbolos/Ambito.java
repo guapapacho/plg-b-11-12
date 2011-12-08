0f82ba90a869cf9b8c19165f4fda9c499c56ee43
tinkaris <tinkaris@44a6b4fc-d1bf-9eb8-79f6-09121c05ef92>
2011-12-08 18:39:41 +0000

    Tabla de simbolos

--- trunk/Proyecto/src/compilador/tablaSimbolos/Ambito.java
+++ trunk/Proyecto/src/compilador/tablaSimbolos/Ambito.java
@@ -1,33 +1,44 @@
 package compilador.tablaSimbolos;
 
-import java.util.ArrayList;
+import java.util.EnumMap;
 import java.util.Hashtable;
 
 public class Ambito {
 	
+	public enum Atributos {
+		TIPO, 
+		NUMARGS,
+		TIPOARGS,
+		PASOARGS,
+		RETORNO,
+		CONTENIDO
+	}
 	private int continente;
 
+	private Hashtable<String, EnumMap<Atributos, Object>> filaAmbito;
 	
-	private Hashtable<String, ArrayList<Object>> filaAmbito;
-
-
-	public Hashtable<String, ArrayList<Object>> getFilaAmbito() {
-		return filaAmbito;
-	}
-
-	public void setFilaAmbito(Hashtable<String, ArrayList<Object>> filaAmbito) {
-		this.filaAmbito = filaAmbito;
-	}
-
+//	private Hashtable<String, ArrayList<Object>> filaAmbito2;
 
 
 	public int getContinente() {
 		return continente;
 	}
 
+	public Hashtable<String, EnumMap<Atributos,Object>> getFilaAmbito() {
+		return filaAmbito;
+	}
+
+	public void setFilaAmbito(
+			Hashtable<String, EnumMap<Atributos, Object>> filaAmbito) {
+		this.filaAmbito = filaAmbito;
+	}
+
 	public void setContinente(int continente) {
 		this.continente = continente;
 	}
+	
+	
+
 
 	
 
